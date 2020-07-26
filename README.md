67ec89ceed87cc66004cdbb7a4bada9567cb5d2a
cavalost <claudina.avalos@opendeusto.es>
2020-07-26 18:01:42 +0100

    feat: handle event handler in the cron job and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # drinks_cron
+
+## Factsheet
+
+| **Category**              | **Value**                                 |
+| ------------------------- | ---------------------------------------- |
+| **Contact**               | claudina.avalos@opendeusto.es
+| **Language / Framework**  | Node
+| **Deployment type**       | PipeDream
+
+## Configuration
+
+Configuration is via the following environment variables:
+
+| Env var      | Example      | Purpose                   |
+| ------------ | ------------ | ------------------------- |
+| `PIPEDREAM_API_KEY` | `-` | To create a new component using their API |
+| `DRINKS_API` | `https://mock-api.drinks.test.siliconrhino.io` | RHINO MOCK API endpoint |
+| `GMAPS_KEY` | `-` | To obtain the address of an event based on the coords
+| `MONGO_URI` | `mongodb+srv://<username>:<password>@uri` | To fetch and insert data to the DB
+| `MONGO_DB_NAME` | `drinks` | MongoDB database name
+
+
+## Requirements
+Node >= 8
+An account in PipeDream in order to get the API key
+An account in Google to enable GMAPS API services
+Create a MongoDB endpoint with Atlas/other service
+
+## How to run the cron job
+Locally:
+```
+npm run start:local
+```
+Creating a PipeDream component:
+```
+npm run start
+```
+
+### Project description
+
+This project uploads a new PipeDream's component that will run a cron job defined
+in the payload each 24 hours.
+
+The cronjob will fetch all the events from the Rhino drinks' mock API and then will check
+if the event exists in the database and otherwise it will insert it, obtaining the 
+address through the coords using Gmaps Services.
+
+Instead of upload each entire object, I have splitted up the logic into several collections,
+as some data is repeated several times, in order to save space. The logic to join all the data
+will be implemented in the backend.
--- cron-method.js
+++ cron-method.js
@@ -1,13 +1,118 @@
 const getEnvVars = () => {
-    const { NAME } = process.env;
+    const { DRINKS_API, GMAPS_KEY, MONGO_URI, MONGO_DB_NAME } = process.env;
     return {
-        NAME
+        DRINKS_API,
+        GMAPS_KEY,
+        MONGO_URI,
+        MONGO_DB_NAME
     };
 };
 
 const runJob = async function run() {
-    const { NAME } = this.getEnvVars();
-    console.log("Hello " + NAME);
+    const axios = require('axios');
+    const _get = require('lodash/get');
+    const { MongoClient } = require('mongodb');
+    const { Client } = require('@googlemaps/google-maps-services-js');
+
+    const { DRINKS_API, GMAPS_KEY, MONGO_URI, MONGO_DB_NAME } = this.getEnvVars();
+    let mongoDbClient, db;
+
+    const fetchEvents = async (pageNumber = 1, pageSize = 10) =>
+        (await axios.get(`${DRINKS_API}/events?page=${pageNumber}&pageSize=${pageSize}`)).data;
+
+    const fetchAllEvents = async () => {
+        const pageSize = 10;
+        let pageNumber = 0;
+        let events = [];
+        do {
+            const newEvents = await fetchEvents(++pageNumber);
+            events = [ ...events, ...newEvents];
+        } while (events.length === (pageSize * pageNumber));
+        return events;
+    };
+
+    const getAddress = async ({ latitude: lat, longitude: lng, name}) => {
+        const client = new Client({});
+        let address;
+        const respCode = await client
+            .reverseGeocode({
+                params: {
+                    latlng: { lat, lng },
+                    key: GMAPS_KEY
+                }
+            });
+        const { compound_code } = _get(respCode, 'data.plus_code', {});
+        if (compound_code) {
+            const respAddress = await client.geocode({
+                params: {
+                    address: compound_code + ' ' + name,
+                    key: GMAPS_KEY
+                },
+            });
+            address = _get(respAddress, 'data.results.0.formatted_address');
+        }
+        return address;
+    };
+
+    const fetchLocation = async location => {
+        let locationDocument = await db.collection('locations')
+            .findOne({ latitude: location.latitude, longitude: location.longitude });
+        if (!locationDocument) {
+            const address = await getAddress(location);
+            if (address) {
+                location.address = address;
+            }
+            ({ ops: [locationDocument] } = await db.collection('locations').insertOne(location));
+        }
+        return locationDocument;
+    };
+
+    const fetchUser = async user => {
+        let userDocument = await db.collection('users')
+            .findOne({ name: user.name });
+        if (!userDocument) {
+            ({ ops: [userDocument] } = await db.collection('users').insertOne(user));
+        }
+        return userDocument;
+    };
+
+    const fetchComment = async comment => {
+        comment.user = (await fetchUser(comment.user))._id;
+        let commentDocument = await db.collection('comments')
+            .findOne({ timestamp: comment.timestamp, user: comment.user });
+        if (!commentDocument) {
+            ({ ops: [commentDocument] } = await db.collection('comments').insertOne(comment));
+        }
+        return commentDocument;
+    };
+
+    try {
+        console.log('Job starts');
+        mongoDbClient = await MongoClient.connect(MONGO_URI, { useNewUrlParser: true });
+        db = mongoDbClient.db(MONGO_DB_NAME);
+        const events = await fetchAllEvents();
+        for (const { location = {}, comments = [], guests = [], ...restEvent } of events) {
+            const eventDocument = await db.collection('events').findOne({ id: restEvent.id });
+            if (!eventDocument) {
+                restEvent.locationId = (await fetchLocation(location))._id;
+                restEvent.creator = (await fetchUser(restEvent.creator))._id;
+                restEvent.guests = [];
+                for (const guest of guests) {
+                    restEvent.guests.push((await fetchUser(guest))._id);
+                }
+                restEvent.comments = [];
+                for (const comment of comments) {
+                    restEvent.comments.push((await fetchComment(comment))._id);
+                }
+                await db.collection('events').insertOne(restEvent);
+            }
+        }
+    } catch (error) {
+        console.log({error});
+    } finally {
+        await mongoDbClient.close();
+        console.log('Job ends');
+    }
 };
 
 module.exports = {
